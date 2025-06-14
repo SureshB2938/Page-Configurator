e2fa251ab1c970af602ee7ba2b582bdd32e11f30
SureshB2938 <sure8928@gmail.com>
2025-06-14 13:33:58 +0530

    Create PdfSelectorApplication.java

---
+++ src/main/java/com/pdfselector/PdfSelectorApplication.java
@@ -0,0 +1,11 @@
+package com.example.pdfselector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfSelectorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PdfSelectorApplication.class, args);
+    }
+}
